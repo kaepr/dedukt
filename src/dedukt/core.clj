926ba608a6618f3d52b886f0b81a419c4875b06d
Shagun Agrawal <agrawalshagun07@gmail.com>
2025-02-10 22:54:01 +0530

    Implementation for super simple querying case.
    Just checks for entity and attribute by value, and returns the value if found.

--- src/dedukt/core.clj
+++ src/dedukt/core.clj
@@ -1,6 +1,43 @@
 (ns dedukt.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(def db
+  [[11 :user/name "richhickey"]
+   [22 :user/name "tonsky"]
+   [33 :user/name "pithyless"]])
+
+(def q
+  [:find '?name
+   :where
+   [11 :user/name '?name]])
+
+(defn parse-query
+  "Returns query in map form.
+
+  Each clause becomes a key inside map."
+  [q]
+  (let [find-index (.indexOf q :find)
+        where-index (.indexOf q :where)
+        vars (subvec q (inc find-index) where-index)
+        wheres (subvec q (inc where-index))]
+    {:find vars
+     :where wheres}))
+
+(defn match
+  "Returns the first matching fact and where clause."
+  [[e a v :as _fact] where-clauses]
+  (first
+   (filter
+    (fn [[we wa]]
+      (and (= e we)
+           (= a wa)))
+    where-clauses)))
+
+(defn run [db q]
+  (let [{:keys [find where]} (parse-query q)
+        rf (fn [acc fact]
+             (if-let [matched (match fact where)]
+               (conj acc [(nth fact 2)])
+               acc))]
+    (reduce rf #{} db)))
+
+(run db q)
